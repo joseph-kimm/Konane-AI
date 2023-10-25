3d584290d55ae2b9ffa5eda33cd47a5b4077b46f
jopopapi <106134415+jopopapi@users.noreply.github.com>
2023-10-24 23:26:54 -0400

    multiple jump

--- Board.py
+++ Board.py
@@ -23,34 +23,79 @@ class Board:
     def remove(self, row, col):
         r = row - 1
         c = col - 1
+        temp = self.pieces[r][c]
         self.pieces[r][c] = '.'
         self.empty.append((r, c))
+        return temp
 
     # Move a piece from start position (r1, c1) to end position at (r2, c2)
     def move(self, r1, c1, r2, c2):
         print("Move (" + str(r1) + "," + str(c1) + ") to (" + str(r2) + "," + str(c2) + ").")
         print()
+
         # determine if moving X or O
         player = self.pieces[r1-1][c1-1]
         
+        # removing the moved piece
         self.remove(r1, c1)
 
-        # Remove piece between start and end. Put player's piece at end position.
-        if (r1-r2 == -2):
-            self.remove(r1+1, c1)
-        if (r1-r2 == 2):
-            self.remove(r2+1, c1)
-        if (c1-c2 == -2):
-            self.remove(r1, c1+1)
-        if (c1-c2 == 2):
-            self.remove(r1, c2+1)
+        # moving the piece
         self.pieces[r2-1][c2-1] = player
         self.empty.remove((r2-1, c2-1))
 
+        # Remove piece between start and end. Put player's piece at end position.
+
+        #going right
+        if (r1-r2 == -2):
+            other = self.remove(r1+1, c1)
+
+            # there is another valid move to the right
+            if Board.isValid(r2+1, c1-1) and self.pieces[r2][c1-1] == other and self.pieces[r2+1][c1-1] == '.':
+                return [r2, c1, r2+2, c1]
+            else:
+                return None
+            
+        # going left
+        if (r1-r2 == 2):
+            other = self.remove(r2+1, c1)
+
+            # there is another valid move to the left
+            if Board.isValid(r2-3, c1-1) and self.pieces[r2-2][c1-1] == other and self.pieces[r2-3][c1-1] == '.':
+                return [r2, c1, r2-2, c1]
+            
+            # there is no other valid move to the left
+            else:
+                return None
+
+        # going down
+        if (c1-c2 == -2):
+            other = self.remove(r1, c1+1)
+
+            # there is another valid moving down
+            if Board.isValid(r1-1, c2+1) and self.pieces[r1-1][c2] == other and self.pieces[r1-1][c2+1] == '.':
+                return [r1, c2, r1, c2+2]
+            
+            # there is no other valid moving down
+            else:
+                return None
+
+        # going up
+        if (c1-c2 == 2):
+            other = self.remove(r1, c2+1)
+
+            # there is another valid moving up
+            if Board.isValid(r1-1, c2-3) and self.pieces[r1-1][c2-2] == other and self.pieces[r1-1][c2-3] == '.':
+                return [r1, c2, r1, c2-2]
+            
+            # there is no other valid moving up
+            else:
+                return None
+        
     # Pick a random move from the provided list
     def randomMove(self, list):
             idx = random.randint(0, len(list)-1)
-            self.move(list[idx][0][0], list[idx][0][1], list[idx][1][0], list[idx][1][1])
+            moves = self.move(list[idx][0][0], list[idx][0][1], list[idx][1][0], list[idx][1][1])
+            return moves
         
 
     # checking if row and column is valid
@@ -98,7 +143,7 @@ class Board:
                 row2 = r+self.DIR2[i][0]
                 col2 = c+self.DIR2[i][1]
 
-                # checking if farther one is valdi
+                # checking if farther one is valid
                 if Board.isValid(row2, col2):
                     
                     #increment count wherever 'X' and 'O' in line
--- Konane.py
+++ Konane.py
@@ -1,5 +1,8 @@
 import time
+import random
 from Board import Board
+
+random.seed(time.clock())
 
 count = 0
 game = Board()
@@ -12,7 +15,6 @@ while True:
     # computer's turn
     if count%2 == 0:
 
-        print("\nComputer's Turn: ", end = " ")
         moves = game.listXMoves()
 
         # check if game is over
@@ -20,19 +22,37 @@ while True:
             print("Player Won")
             exit()
         
+        print("\nComputer's Turn: ", end = " ")
+
         # pause, then make move
         time.sleep(1)
-        game.randomMove(moves)
+        next = game.randomMove(moves)
+
+        time.sleep(1)
+        game.print()
+        time.sleep(1)
+
+        # checking if there are additional moves to be made
+        while next:
+            choice = random.randint(0,1)
+
+            if choice == 0:
+                next = move(next[0], next[1], next[2], next[3])
+                time.sleep(1)
+                print()
+                game.print()
+                time.sleep(1)
     
     # player's turn
     else:
-        print("\nUser's Turn\n")
         moves = game.listOMoves()
 
         # check if game is over
         if len(moves) == 0:
             print("Computer Won")
             exit()
+
+        print("\nUser's Turn\n")
 
         # print all the available moves
         print("Available moves:")
@@ -41,12 +61,22 @@ while True:
 
         move = int(input("Enter the number of the move that you want to make: ")) -1
 
-        # print(moves[move][0][0], moves[move][0][1], moves[move][1][0], moves[move][1][1])
+        next = game.move(moves[move][0][0], moves[move][0][1], moves[move][1][0], moves[move][1][1])
 
-        game.move(moves[move][0][0], moves[move][0][1], moves[move][1][0], moves[move][1][1])
-    time.sleep(1)
-    game.print()
-    time.sleep(1)
+        time.sleep(1)
+        game.print()
+        time.sleep(1)
+
+        # checking if there are additional moves to be made
+        while next:
+            play = input(f"Do you want to make another move from {(next[0], next[1])} to {(next[2], next[3])}? (y/n): ")
+            if play == 'y':
+                next = game.move(next[0], next[1], next[2], next[3])
+                time.sleep(1)
+                print()
+                game.print()
+                time.sleep(1)
+
     count += 1
 
 
